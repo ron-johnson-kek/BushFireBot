85d2068d96db9542d89d35658c850aec47ad1f06
ron-johnson-kek <wilding9942@gmail.com>
2022-06-10 18:05:27 -0700

    added commands

--- app.js
+++ app.js
@@ -6,8 +6,14 @@ const blankchar = '󠀀';
 const { ChatClient } = require("dank-twitch-irc");
 const { resourceLimits } = require("worker_threads");
 const startTimeStamp = Date.now();
-
+//Filepaths
+const configFilePath = 'config1.json';
+const activeFilepath = 'ActiveData.json';
+const userdataFilepath = 'userdata.json';
+const hotspotFilepath = 'HotSpotData.json';
 //Config Access
+let username = '';
+let password = '';
 try {
   let configData = readDataJson(configFilePath);
   username = configData["username"];
@@ -17,8 +23,6 @@ try {
   console.log("Error, could not read config/channels file. Quitting");
   process.exit(1);
 }
-let username = '';
-let password = '';
 
 //Client Access
 let client = new ChatClient({
@@ -38,15 +42,10 @@ client.on("close", (error) => {
 
 //Global Variables
 const trusted = ['ron__johnson_'];
-
+const usernameList = {"data": []};
 //URL's
 const activefiresURL = 'https://cwfis.cfs.nrcan.gc.ca/downloads/activefires/activefires.csv';
 const hotspotURL = 'https://cwfis.cfs.nrcan.gc.ca/downloads/hotspots/hotspots.csv';
-//Filepaths
-const configFilePath = 'config1.json';
-const activeFilepath = 'ActiveData.json';
-const userdataFilepath = 'userdata.json';
-const hotspotFilepath = 'HotSpotData.json';
 //getCSV's
 getCSV(activefiresURL, 'ActiveData');
 getCSV(hotspotURL, 'HotspotData');
@@ -86,17 +85,23 @@ client.on("PRIVMSG", async (msg, channelName, self) => {
   
   let hotspotData = readDataJson2(hotspotFilepath);
   let activeData = readDataJson2(activeFilepath);
-  let userdata = readDataJson2(userdataFilepath);
+  //let userdata = readDataJson2(userdataFilepath);
+  let numactivefires = Object.keys(activeData).length;
 
-  const usernameList = [];
-
+  if (isCommand(cleanMessage, 'fireforecast')) {
+    let usercountry = cleanMessage.substring(14);
+    let cleanusercountry = usercountry.toLowerCase();
+    client.say(msg.channelName, `${getCountry(cleanusercountry)}`)
+  }
+  if (isCommand(cleanMessage, 'activefires')) {
+    client.say(msg.channelName, `${rateNumactivefires(numactivefires)}`)
+  }
   if (isCommand(cleanMessage, 'bigfires')) {
     activeData.forEach(Element => {
       if (Element[" hectares"] > 10000) {
       client.say(msg.channelName, `${Element[" firename"]} in ${Element.agency} is over 10000 ha currently at ${Element[" hectares"]} ha monkaS https://earth.google.com/web/search/${Element[" lat"]},+${Element[" lon"]}/`)
       }
     });
-    
   }
 //wip
   if (trusted.includes(msg.displayName) && isCommand(cleanMessage, 'hotspots')) {
@@ -105,12 +110,11 @@ client.on("PRIVMSG", async (msg, channelName, self) => {
       client.say(msg.channelName, `Hotspot located at ${Element["lat"]}, ${Element["lon"]} has crown fire potential monkaOMEGA (that means it burns the entire tree)`);
       }
     })
-      
   }
 //wip
   if (trusted.includes(msg.displayName) && isCommand(cleanMessage, 'addUser')) {
-    
-    FileSystem.writeFile(`./userdata.json`, username, err => {
+    //const usernamePush = usernameList["data"].push(`{"${msg.displayName}"}`);
+    FileSystem.writeFile(`./userdata.json`, usernamePush, err => {
       if(err){
         console.log("Error writing file" ,err)
       } else {
@@ -156,4 +160,22 @@ function getCSV(csvURL, filename) {
         }
     });
 });
+}
+
+function rateNumactivefires(num) { 
+  return num > 40 && num < 50 ? `There are ${num} active fires in North America KKonaW`
+  : num > 50 && num < 80 ? `There are ${num} active fires in Canada, be careful out there! KKonaW 🔥`
+  : num > 80 && num < 100 ? `There are ${num} active fires in Canada, be careful out there! KKonaW o 0 ( monkaS 🔥 )`
+  : num > 100 ? `There are ${num} active fires in Canada, stay inside monkaOMEGA` 
+  : `There are ${num} active fires in North America PagMan`
+  
+}
+
+function getCountry(country) {
+  let thisMonth = new Date();
+  let date = (thisMonth.getMonth()+1);
+  return country === 'canada'? `Here is the seasonal forecast for Canada, updated monthly FeelsOkayMan https://cwfis.cfs.nrcan.gc.ca/data/maps/seasonal_forecast/2022/fsr0${date}.png`
+  : country === 'usa' || country === 'united states' ? 'Here is the seasonal forecast for the USA'
+  : country === 'uk' || country === 'united kingdom' ? 'Here is the seasonal forecast for the UK'
+  : 'Unfortunately I currently do not support the country provided Sadeg'
 }
--- commands.md
+++ commands.md
@@ -4,4 +4,6 @@
 - code: Gives link to GitHub
 - commands: ...
 - bigfires: relays any active fire over 10000 ha and its location on google earth
+- activefires: provides raw number of active fires in Canada and Alaska
+- fireforecast: gives user the most up to date forecast for a given country. Currently accepted countries are: canada, usa, uk.
 - quit: :tf:
